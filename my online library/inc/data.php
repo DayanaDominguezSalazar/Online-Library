b37362c63abf4ea02a58eb4015835f4852d42e31
DayanaDominguezSalazar <50426605+DayanaDominguezSalazar@users.noreply.github.com>
2019-06-03 18:12:11 -0400

    Add files via upload

---
+++ my online library/inc/data.php
@@ -0,0 +1,175 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "each little bird that sings",
+	"img" => "img/media/bird.png",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 2005,
+    "category" => "Books",
+    "authors" => [
+        "Deborah Wiles"
+    ],
+    "publisher" => "Houghton Mifflin Harcourt",
+    "isbn" => '9780152056575'
+];
+$catalog[102] = [
+    "title" => "Ayna's Ghost",
+    "img" => "img/media/anya.png",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 2012,
+    "category" => "Books",
+    "authors" => [
+        "Vera Brosgol"
+    ],
+    "publisher" => "Square Fish",
+    "isbn" => '9781250040015'
+];
+$catalog[103] = [
+    "title" => "Fahrenheit 451",
+    "img" => "img/media/fahrenheit.png",
+    "genre" => "Science Fiction, Dystopian Fiction, Political fiction",
+    "format" => "Paperback",
+    "year" => 1953,
+    "category" => "Books",
+    "authors" => [
+        "Ray Bradbury"
+    ],
+    "publisher" => "Simon & Schuster",
+    "isbn" => '9781451673319'
+];
+$catalog[104] = [
+    "title" => "Never Cry Wolf",
+    "img" => "img/media/wolf.png",
+    "genre" => "fiction",
+    "format" => "Paperback",
+    "year" => 2001,
+    "category" => "Books",
+    "authors" => [
+        "Farley Mowat"
+    ],
+    "publisher" => "Little, Brown and Company",
+    "isbn" => '9780316881791'
+];
+//Movies
+$catalog[201] = [
+    "title" => "A Dog's Way Home",
+    "img" => "img/media/dogs.png",
+    "genre" => "Drama/Adventure",
+    "format" => "DVD",
+    "year" => 2019,
+    "category" => "Movies",
+    "director" => "Charles Martin Smith",
+    "writers" => [
+        "W. Bruce Cameron",
+        "Cathryn Michon"
+    ],
+    "stars" => [
+        "Ashley Judd",
+        "Jonah Hauer-King",
+        "Bryce Dallas Howard",
+    ]
+];
+$catalog[202] = [
+    "title" => "How To Train Your Dragon",
+    "img" => "img/media/dragon.png",
+    "genre" => "Action, Adventure. Fantasy, Drama",
+    "format" => "DVD",
+    "year" => 2010,
+    "category" => "Movies",
+    "director" => "Dean DeBlois",
+	               "Chris Sanders",
+    "writers" => [
+         "Cressida Cowell"
+    ],
+    "stars" => [
+			"Jay Baruchel",
+      "Gerard Butler",
+      "Craig Ferguson",
+      "America Ferrera"
+    ]
+];
+$catalog[203] = [
+    "title" => "My Hero Academia: Two Heroes",
+    "img" => "img/media/hero.png",
+    "genre" => "Action/Superhero",
+    "format" => "DVD",
+    "year" => 2018,
+    "category" => "Movies",
+    "director" => "Kenji Nagasaki",
+    "writers" => [
+        "Guy Ritchie",
+        "Lionel Wigram"
+    ],
+    "stars" => [
+			"Alicia Vikander",
+			"Henry Cavill",
+			"Armie Hammer"
+    ]
+];
+$catalog[204] = [
+    "title" => "Avengers: Infinity War",
+		"img" => "img/media/avengers.png",
+	  "genre" => "Fantasy/Sci-fi",
+	  "format" => "DVD",
+	  "year" => 2018,
+	  "category" => "Movies",
+	  "director" => "Anthony Russo",
+	                 "Joe Russo",
+	  "writers" => [
+			"Stan Lee",
+			"Christopher Markus",
+			"Stephen Mcfeely",
+			"Jack Kirby"
+
+	  ],
+	  "stars" => [
+			"Robert Doweny Jr.",
+      "Chris Hemsworth",
+      "Mark Ruffalo",
+      "Benedict Cumberbatch",
+      "Tom Holland"
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "Porter Robinson: Worlds",
+	"img" => "img/media/porter.png",
+	"genre" => "Electronic dance music, Synth-pop, Chillwave, Dream pop",
+	"format" => "CD",
+	"year" => 2014,
+	"category" => "Music",
+	"artist" => "Porter Robinson"
+];
+
+$catalog[302] = [
+	"title" => "Hikaru Utada: Face My Fears",
+	"img" => "img/media/hikaru.png",
+	"genre" => "Dance/Electronic",
+	"format" => "Digital",
+	"year" => 2019,
+	"category" => "Music",
+	"artist" => "Hikaru Utada",
+	  					"Skrillex"
+];
+$catalog[303] = [
+	"title" => "Regina Spektor: Far",
+	"img" => "img/media/regina.png",
+	"genre" => "Alternative/Indie",
+	"format" => "CD",
+	"year" => 2009,
+	"category" => "Music",
+	"artist" => "Regina Spektor"
+];
+$catalog[304] = [
+	"title" => "Heathers",
+	"img" => "img/media/heathers.png",
+	"genre" => "Rock Musical",
+	"format" => "Musical",
+	"year" => 2010,
+	"category" => "Music",
+	"artist" => "Laurence O'Keefe",
+                 "Kevin Murphy"
+];
---
+++ my online library/inc/dataz.php
@@ -0,0 +1,135 @@
+<?php
+
+///Books
+$catalog[101] = [
+  "title" => "1984",
+  "img" => "img/1984.jpg",
+ "genre" => "Science-fiction",
+ "format" => "Paperback",
+ "year" => 1949,
+ "category" => "Books",
+ "authors" => [
+     "George Orwell"
+ ],
+ "publisher" => "Harvill Secker",
+ "isbn" => '9781497490826'
+];
+
+$catalog[102] = [
+  "title" => "The Lord of the Flies",
+  "img" => "img/flies.jpg",
+ "genre" => "Allegory",
+ "format" => "Paperback",
+ "year" => 1954,
+ "category" => "Books",
+ "authors" => [
+     "William Golding"
+ ],
+ "publisher" => "Faber and Faber",
+ "isbn" => '9780399501487'
+];
+
+$catalog[103] = [
+  "title" => "The Hunger Games",
+  "img" => "img/hunger.jpg",
+ "genre" => "Science-Fiction",
+ "format" => "Paperback",
+ "year" => 2008,
+ "category" => "Books",
+ "authors" => [
+     "Suzanne Collins"
+ ],
+ "publisher" => "Scholastic Corporation",
+ "isbn" => '9788852023729'
+];
+
+///Movies
+$catalog[104] = [
+ "title" => "Archie's Final Project",
+ "img" => "img/archie.jpg",
+ "genre" => "Comedy,Drama",
+ "format" => "DVD",
+ "year" => 2009,
+ "category" => "Movies",
+ "director" => "David Lee Miller",
+ "writers" => [
+     "Eric J. Adams",
+     "David Lee Miller"
+
+ ],
+ "stars" => [
+     "David Carradine",
+     "Mariel Hemingway",
+     "Brooke Nevin"
+ ]
+];
+
+$catalog[105] = [
+ "title" => "Brick",
+ "img" => "img/brick.jpeg",
+ "genre" => "Action,Crime",
+ "format" => "DVD",
+ "year" => 2005,
+ "category" => "Movies",
+ "director" => "Rian Johnson",
+ "writers" => [
+     "Rian Johnson"
+
+ ],
+ "stars" => [
+     "Joseph Gordon-Levitt",
+     "Lukas Haas",
+     "Emilie de Ravin"
+ ]
+];
+
+$catalog[106] = [
+ "title" => "Nick &amp; Norah's Infinite Playlist",
+ "img" => "img/nn.jpg",
+ "genre" => "Comedy,Drama",
+ "format" => "DVD",
+ "year" => 2008,
+ "category" => "Movies",
+ "director" => "Peter Sollett",
+ "writers" => [
+     "Lorene Scafaria",
+     "Rachel Cohn"
+
+ ],
+ "stars" => [
+     "Michael Cera",
+     "Kat Dennings",
+     "Aaron Yoo"
+ ]
+];
+
+///Music
+$catalog[107] = [
+ "title" => "Lean Into It",
+ "img" => "img/allison.jpg",
+ "genre" => "Pop,Punk,Lo-Fi",
+ "format" => "CD",
+ "year" => "2014",
+ "category" => "Music",
+ "artist" => "Allison Crutchfield"
+];
+
+$catalog[108] = [
+ "title" => "Pottymouth",
+ "img" => "img/brat.jpg",
+ "genre" => "Punk Rock,Riot grrrl,Indie-rock",
+ "format" => "CD",
+ "year" => "1993",
+ "category" => "Music",
+ "artist" => "Bratmobile"
+];
+
+$catalog[109] = [
+ "title" => "Going Grey",
+ "img" => "images/album/tfb.jpeg",
+ "genre" => "Alternative,Indie",
+ "format" => "CD",
+ "year" => "2017",
+ "category" => "Music",
+ "artist" => "The Front Bottoms"
+];
